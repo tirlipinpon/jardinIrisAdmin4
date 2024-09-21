fbf50d6fd42f2090b93383e2b5b99aa87fff025b
Tony <tirlipinpon@hotmail.com>
2024-09-21 10:13:54 +0200

    added add post html and form

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'add-new-post',
+    loadComponent: () => import('./posts/post-add/post-add.component').then(m => m.PostAddComponent), title: 'add-new-post'},
+  { path: 'get-all-post',
+    loadComponent: () => import('./posts/post-all/post-all.component').then(m => m.PostAllComponent), title: 'get-all-post',
+    children: [
+      {
+        path: 'edit-old-post/:id', // child route path
+        loadComponent: () => import('./posts/post-edit/post-edit.component').then(m => m.PostEditComponent),
+        title: 'edit-old-post'
+      }
+    ]},
+  { path: '**', loadComponent: () => import('./posts/post-all/post-all.component').then(m => m.PostAllComponent)
+  }
+];
